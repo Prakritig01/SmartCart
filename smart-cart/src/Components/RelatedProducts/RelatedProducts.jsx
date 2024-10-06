da42b71686a3fd2df8fce00b34a3333176377ab6
Prakritig01 <prakritigupta1112002@gmail.com>
2024-10-06 21:42:30 +0530

    related products component added

---
+++ smart-cart/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { ShopContext } from "../../Context/Shopcontext";
+import all_products from "../Assests/all_products";
+import Item from "../Item/Item";
+import './RelatedProducts.css';
+
+
+function getRelatedProductsFromCategory(category,all_products) {
+    const same_category_products = all_products.filter(product=> product.category == category);
+    console.log("same category products:");
+    console.log(same_category_products);
+    const len = same_category_products.length;
+    console.log(len);
+    //generate 4 numbers between range len
+    let random_set = new Set();
+    const related_products = [];
+    const related_products_ids = [];
+    while(random_set.size <4 && same_category_products.length > 0){
+        const random_num = Math.floor(Math.random()*len);
+        if(random_set.has(random_num)){
+            continue;
+        }
+        random_set.add(random_num);
+        related_products.push(same_category_products[random_num]);
+        related_products_ids.push(same_category_products[random_num].id);
+
+    }
+    console.log("related products:",related_products);
+    console.log("related_products_id",related_products_ids);
+    return related_products;
+}
+
+const RelatedProducts = ({category}) => {
+    const all_products = useContext(ShopContext);
+    //ab humei all_products mei se related products vaala array chahiye jiske liyehumne alag function likh diya
+    const related_products_array = getRelatedProductsFromCategory(category,all_products);
+    return ( 
+        <div className="relatedproducts">
+            <h1>Related Products</h1>
+            <hr />
+            <div className="relatedproducts-items">
+                {related_products_array.map(item =>{
+                    return <Item key = {item.id} item = {item} />
+                })}
+            </div>
+        </div>
+     );
+}
+ 
+export default RelatedProducts;
--- smart-cart/src/Pages/Product.jsx
+++ smart-cart/src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { ShopContext } from "../Context/Shopcontext";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../Components/Description_box/Description";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 const Product = () => {
   const { id } = useParams();
   const all_products = useContext(ShopContext);
@@ -12,12 +13,14 @@ const Product = () => {
   const single_product_array = all_products.filter((product) => product.id == id);
   const single_product = single_product_array[0];
   console.log(single_product);
+  console.log(single_product.category);
   return (
     <>
       {/* <h1>Product page : {id}</h1> */}
       <Breadcrumb product={single_product} />
       <ProductDisplay product={single_product} />
       <DescriptionBox/>
+      <RelatedProducts category = {single_product.category} />
     </>
   );
 };
