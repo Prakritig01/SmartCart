597790dab852515169b4d2bc0e86424d32e106f2
Prakritig01 <prakritigupta1112002@gmail.com>
2024-10-06 17:15:04 +0530

    a tag changed to link in breadcrum

--- smart-cart/src/Components/Breadcrumb/Breadcrumb.jsx
+++ smart-cart/src/Components/Breadcrumb/Breadcrumb.jsx
@@ -1,15 +1,21 @@
-import breadcrum_arrow from './../Assests/breadcrumb/breadcrum_arrow.png';
-import './Breadcrum.css';
-const Breadcrumb = ({product}) => {
-    return ( 
-        <div className="breadcrum">
-            <a href="/">HOME</a>
-            <img src= {breadcrum_arrow} alt="bread_crum" />
-            <a href= {`/${product.category.toLowerCase()}`}> {`${product.category.toUpperCase()}`}</a>
-            <img src= {breadcrum_arrow} alt="bread_crum" />
-            <span>{product.name}</span>
-        </div>
-     );
-}
- 
+import breadcrum_arrow from "./../Assests/breadcrumb/breadcrum_arrow.png";
+import "./Breadcrum.css";
+import { Link } from "react-router-dom";
+const Breadcrumb = ({ product }) => {
+  return (
+    <div className="breadcrum">
+      
+      <Link to="/">HOME</Link>
+      <img src={breadcrum_arrow} alt="bread_crum" />
+
+      <Link Link to={`/${product.category.toLowerCase()}`}>
+        {" "}
+        {`${product.category.toUpperCase()}`}
+      </Link>
+      <img src={breadcrum_arrow} alt="bread_crum" />
+      <span>{product.name}</span>
+    </div>
+  );
+};
+
 export default Breadcrumb;
